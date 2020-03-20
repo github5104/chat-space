1e7076d72e7ce72990bbd71b70f166089cf25206
github5104 <gaaliker@gmail.com>
2020-03-20 12:56:39 +0900

    change setinterval

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -110,6 +110,6 @@ $(function () {
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 1000);
+    setInterval(reloadMessages, 7000);
   }
 });
